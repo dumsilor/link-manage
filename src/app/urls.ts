183c19705c2ed837b1147e907b4c8770976d463f
Mosiur Rahman <mosiur.rahman@novotel-bd.com>
2024-11-17 15:10:16 +0600

    Completed

---
+++ src/app/urls.ts
@@ -0,0 +1,94 @@
+import { Tool } from "./shared/model/tool.model";
+
+export const all_tools: Tool[] = [
+{
+    name: 'Nagios',
+    url: "http://nagios.brilliant.com.bd/nagios",
+    userName: "nagiosadmin",
+    password: "Icloud-#@!!@#"
+},
+{
+    name: "Graphana",
+    url: "https://grafana.brilliant.com.bd/login",
+    userName: "cloud-operation",
+    password: "Br!ll!ant-#@!!@#",
+},
+{
+    name: "Cacti",
+    url: "http://cacti.brilliant.com.bd/cacti",
+    userName: "admin",
+    password: "Icl0ud#@!",
+},
+{
+   name: "Fortigate",
+   url: "https://118.67.210.46/",
+   userName: "cloud-operation",
+   password: "ic-FW-Cl0ud-ops132267"
+},
+{
+    name: "Forti Analyzer",
+    url: "https://118.67.209.115/",
+    userName: "cloud-operation",
+    password: "ic-FW-Cl0ud-ops132267"
+ },
+ {
+    name: "Observium",
+    url: "http://observium.brilliant.com.bd/",
+    userName: "admin",
+    password: "Icloud-#@!!@#"
+ },
+ {
+    name: "VM 1",
+    url: "http://118.67.215.11/dashboard/",
+    userName: "mosiur.rahman",
+    password: " brilliant-mosiur.rahman"
+ },
+ {
+    name: "VM 2",
+    url: "http://118.67.215.12/dashboard/",
+    userName: "mosiur.rahman",
+    password: " brilliant-mosiur.rahman"
+ },
+ {
+    name: "VM 3",
+    url: "http://118.67.215.13/dashboard/",
+    userName: "mosiur.rahman",
+    password: " brilliant-mosiur.rahman"
+ },
+ {
+    name: "VM 4",
+    url: "https://cloud.brilliant.com.bd/",
+    userName: "mosiur.rahman",
+    password: " brilliant-mosiur.rahman"
+ },
+ {
+    name: "BCP Portal",
+    url: "https://bcp.brilliant.com.bd/",
+    userName: "mosiur.rahman@brilliant.com.bd",
+    password: "brilliant-mosiur.rahman"
+ },
+ {
+   name: "Cloud File Serve",
+   url: "http://118.67.215.251",
+   userName: "cloud-support",
+   password: "Icl0ud-Supp0rt!@#"
+ },
+ {
+   name: "CA Admin",
+   url: " https://ca.brilliant.com.bd",
+   userName: "admin@brilliant.com.bd",
+   password: "@Dm!NBr!ll!@nt"
+ },
+ {
+   name: "BCP Portal",
+   url: "http://118.67.215.13/dashboard/",
+   userName: "core-system",
+   password: "F91@cPR20L!kRe0651096hjQ#2072"
+},
+{
+   name: "CommVault Support Portal",
+   url: "https://support.commvault.com/",
+   userName: "core.system@intercloud.com.bd",
+   password: "mBSrdN&I/d90"
+},
+]
---
+++ test.py
@@ -0,0 +1,7 @@
+# Open a file in write mode
+with open("example.txt", "w") as file:
+    # Write data to the file
+    file.write("Hello, world!\n")
+    file.write("This is a new line.\n")
+
+print("File written successfully!")
