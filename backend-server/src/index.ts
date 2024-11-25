ceacc20f68a0c853d1a6a5d56b28759a50da9ac1
Mosiur <dum.silor@gmail.com>
2024-11-26 00:20:05 +0600

    Editing DB

--- backend-server/src/index.ts
+++ backend-server/src/index.ts
@@ -1,31 +1,38 @@
 import express from "express";
 import cors from 'cors';
 import mongoose from 'mongoose';
+import {vols} from "../../src/app/backup"
 
 const app = express()
 
 app.use(express.json())
 
 
-app.connect('mongodb://118.67.213.117:27017/navigatorDB')
+mongoose.connect('mongodb://jutsujen:f%40!ry-T%40!l@118.67.213.117:27017/navigatorDB?authMechanism=DEFAULT&tls=false').then(()=>{console.log("Connected")}).catch((err)=>console.log(err))
 
 const headerSchema = new mongoose.Schema({
     headerName: String
 })
 
-const backupSchema =  new mongoose.Schema({
+interface IBackup {
+    projectName: string,
+    projectURL: string,
+    volumeName: string,
+    volumeURL: string
+}
+
+const backupSchema =  new mongoose.Schema<IBackup>({
     projectName: String,
     projectURL: String,
     volumeName: String,
     volumeURL: String
 })
 
-const Backup = new mongoose.Model("backup",backupSchema)
+const Backup =  mongoose.model<IBackup>("backup",backupSchema)
 
-app.get("/seed-backup", (req,res)=>{
-    const newBackup = new Backup({
-
-    })
+app.get("/seed-backup", async (req,res)=>{
+    const result = await Backup.insertMany(vols);
+    res.status(200).send(`Successfully seeded ${result.length} records.`);
 })
 
 app.get("/", (req,res)=>{
