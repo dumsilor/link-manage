5af95f296a517f88a81e99faa9c92472513e5d90
Mosiur Rahman <mosiur.rahman@novotel-bd.com>
2025-01-05 00:21:30 +0600

    Login Component added

---
+++ src/app/components/pages/login/login.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
