f5a86a0b5d74b31e518e00307237c01e882512bc
Mosiur Rahman <mosiur.rahman@novotel-bd.com>
2025-01-05 00:36:37 +0600

    Login Page partially Completed

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +9,35 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor() {}
+
+  loginForm = new FormGroup({
+    userName: new FormControl(''),
+    password: new FormControl('')
+  })
+
+  sample_user = [{
+    userName: 'admin',
+    password: 'admin'
+  },
+{
+  userName: 'rashik',
+  password: '123'
+}]
+
+  ngOnInit() {
+    // do something
+  }
+
+  login() {
+    let username = this.loginForm.value.userName;
+    let password = this.loginForm.value.password;
+    if (this.sample_user.find(user=> user.userName === username && user.password === password)) {
+      console.log('Login Successful');
+    } else {
+      console.log('Login Failed');
+    }
+  }
 
 }
